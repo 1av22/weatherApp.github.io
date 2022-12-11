59a4555b9dce320c66d1320c09adec2ea6a868c3
Aditya Verma <79130324+1av22@users.noreply.github.com>
2022-12-11 12:26:32 +0530

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+// let url = "http://api.weatherapi.com/v1/current.json?key=49372a938fda4449bad82832221012&q={}&aqi=no"
+// °
+const placeHeading = document.querySelector("#placeHeading");
+const weatherImage = document.querySelector("#weatherImage");
+const tempratureData = document.querySelector("#tempratureData");
+const weatherName = document.querySelector("#weatherName");
+const inputbox = document.querySelector("#inputBox");
+const searchbutton = document.querySelector("#searchButton");
+
+const search = () => {
+	let place = inputbox.value;
+	inputbox.value = "";
+	let url = `http://api.weatherapi.com/v1/current.json?key=49372a938fda4449bad82832221012&q=${place}&aqi=no`;
+	const res = axios.get(url);
+	res.then((res) => {
+		const data = res.data;
+		placeHeading.innerText = data.location.name;
+		tempratureData.innerText = `${data.current.temp_c}° C`;
+		weatherName.innerText = data.current.condition.text;
+
+		// image part
+		let iUrl = data.current.condition.icon;
+		let imgUrl = `pics/${iUrl.slice(35)}`;
+		weatherImage.src = imgUrl;
+	});
+};
+
+searchbutton.addEventListener("click", search);
+
+// 34 indexes to be removed.
