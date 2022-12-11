06d305037253e5c8896f825e7cb84f090067d0cd
Aditya Verma <79130324+1av22@users.noreply.github.com>
2022-12-11 13:03:51 +0530

    image update

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ const search = () => {
 		// image part
 		let iUrl = data.current.condition.icon;
 		console.log(iUrl);
-		let imgUrl = `pics/${iUrl.slice(35)}`;
-		imgUrl = imgUrl.slice(49);
-		console.log(imgUrl);
+		let imgUrl = `https://github.com/1av22/weatherApp.github.io/tree/main/pics/${iUrl.slice(35)}`;
 		weatherImage.src = imgUrl;
 	});
 };
